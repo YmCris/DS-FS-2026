9bf8c1e074f4c43b646f082bdbf5d6eda3486234
YmCris <ymcrisystems@gmail.com>
2026-02-17 00:45:41 -0600

    Adding linkedList

--- UNO/SourceCode/include/card_game/core/data_structures/linked_list/LinkedList.h
+++ UNO/SourceCode/include/card_game/core/data_structures/linked_list/LinkedList.h
@@ -5,6 +5,7 @@
 #ifndef SOURCECODE_LINKEDLIST_H
 #define SOURCECODE_LINKEDLIST_H
 
+#include <iostream>
 #include <stdexcept>
 
 #include "SimpleNode.h"
@@ -20,72 +21,231 @@ class LinkedList
 {
 public:
     // LIFETIME
-    explicit LinkedList(T value);
+    LinkedList() = default;
+    LinkedList(const LinkedList&) = delete;
+    LinkedList& operator=(const LinkedList&) = delete;
 
-    // PUBLIC API
-    bool isEmpty()
+    explicit LinkedList(T value)
     {
-        return head_==nullptr;
+        addFirst(value);
     }
 
-    int getSize()
+    ~LinkedList()
+    {
+        while (head_ != nullptr)
+        {
+            SimpleNode<T>* temp = head_;
+            head_ = head_->next();
+            delete temp;
+        }
+    }
+
+    // PUBLIC API
+    bool isEmpty() const
+    {
+        return head_ == nullptr;
+    }
+
+    [[nodiscard]] int getSize() const
     {
         return size_;
     }
 
-    void addAt(T value, int position)
+    void addAt(T& value, const int position)
     {
-        if (position==0)
+        outOfRangeToAdd(position);
+
+        if (position == 0)
         {
             addFirst(value);
             return;
         }
-        if (position==size_-1)
+        if (position == size_)
         {
             addLast(value);
             return;
         }
-        outOfRange();
-    }
 
-    void addFirst(T value)
-    {
-        SimpleNode<T> node = new SimpleNode<T>(value);
-        this->head_= node;
-    }
+        int accountant = 1;
+        SimpleNode<T>* node = new SimpleNode<T>(value);
+        SimpleNode<T>* prev = head_;
 
-    void addLast(T value)
-    {
-        SimpleNode<T> node = new SimpleNode<T>(value);
-        SimpleNode<T> temp = head_;
-        while (temp!=nullptr)
+        while (accountant < position)
         {
-            temp = temp.next();
+            prev = prev->next();
+            accountant++;
+        }
+
+        SimpleNode<T>* next = prev->next();
+
+        prev->setNext(node);
+        node->setNext(next);
+        size_++;
+    }
+
+    void addFirst(T& value)
+    {
+        SimpleNode<T>* node = new SimpleNode<T>(value);
+
+        node->setNext(head_);
+        head_ = node;
+
+        size_++;
+    }
+
+    void addLast(T& value)
+    {
+        SimpleNode<T>* node = new SimpleNode<T>(value);
+        if (head_ == nullptr)
+        {
+            head_ = node;
+        }
+        else
+        {
+            SimpleNode<T>* temp = head_;
+            while (temp->next() != nullptr)
+            {
+                temp = temp->next();
+            }
+            temp->setNext(node);
+        }
+
+        size_++;
+    }
+
+    void removeAt(const int position)
+    {
+        outOfRangeToAccess(position);
+
+        if (position == 0)
+        {
+            removeFirst();
+            return;
+        }
+        if (position == size_ - 1)
+        {
+            removeLast();
+            return;
+        }
+
+        int accountant = 1;
+        SimpleNode<T>* prev = head_;
+        while (accountant < position)
+        {
+            prev = prev->next();
+            accountant++;
+        }
+
+        SimpleNode<T>* post = prev->next()->next();
+        SimpleNode<T>* prePost = prev->next();
+        prePost->setNext(nullptr);
+
+        delete prePost;
+        prev->setNext(post);
+
+        size_--;
+    }
+
+    void removeFirst()
+    {
+        SimpleNode<T>* head = head_;
+        head_ = head->next();
+
+        delete head;
+
+        size_--;
+    }
+
+    void removeLast()
+    {
+        if (head_ == nullptr)
+            return;
+
+        if (head_->next() == nullptr)
+        {
+            delete head_;
+            head_ = nullptr;
+        }
+        else
+        {
+            SimpleNode<T>* current = head_;
+            while (current->next()->next() != nullptr)
+                current = current->next();
+
+            delete current->next();
+            current->setNext(nullptr);
+        }
+
+        --size_;
+    }
+
+
+    SimpleNode<T>* getAt(const int position)
+    {
+        outOfRangeToAccess(position);
+
+        if (size_ == 0) throw std::logic_error("List is empty");
+        if (position == 0) return getHead();
+        if (position == size_ - 1) return getTail();
+
+        int accountant = 1;
+        SimpleNode<T>* node = head_->next();
+        while (accountant < position)
+        {
+            node = node->next();
+            accountant++;
+        }
+
+        return node;
+    }
+
+    SimpleNode<T>* getHead() const
+    {
+        return head_;
+    }
+
+    SimpleNode<T>* getTail() const
+    {
+        SimpleNode<T>* temp = head_;
+        while (temp->next() != nullptr)
+        {
+            temp = temp->next();
+        }
+        return temp;
+    }
+
+    T getContent(SimpleNode<T>* node) const
+    {
+        return node->value();
+    }
+
+    void print() const
+    {
+        SimpleNode<T>* node = head_;
+        while (node != nullptr)
+        {
+            std::cout << node->value() << std::endl;
+            node = node->next();
         }
     }
-
-    void removeAt(int position){}
-    void removeFirst(){}
-    void removeLast(){}
-
-    SimpleNode<T>* getAt(int position){}
-    SimpleNode<T>* getTail(){}
-    SimpleNode<T>* getHead(){}
-    T getContent(SimpleNode<T> node){}
 
 private:
     // HELPER METHODS
-    void outOfRange(const int& position) const
+    void outOfRangeToAdd(const int pos) const
     {
-        if (position<0 || position> size_)
-        {
-            throw std::out_of_range("You have to select a number grater or equals than 0 and less than "+size_);
-        }
+        if (pos < 0 || pos > size_)
+            throw std::out_of_range("Invalid position to add");
+    }
+
+    void outOfRangeToAccess(const int pos) const
+    {
+        if (pos < 0 || pos >= size_)
+            throw std::out_of_range("Invalid position");
     }
 
     // DATA MEMBERS
-    SimpleNode<T>* head_;
-    int size_;
+    SimpleNode<T>* head_ = nullptr;
+    int size_ = 0;
 };
 
 
--- UNO/SourceCode/src/main.cpp
+++ UNO/SourceCode/src/main.cpp
@@ -1,7 +1,132 @@
 #include <iostream>
 
+#include "../include/card_game/core/data_structures/linked_list/LinkedList.h"
+
+void showMenu()
+{
+    std::cout << "\n==== MENU LINKED LIST ====\n";
+    std::cout << "1. Add First\n";
+    std::cout << "2. Add Last\n";
+    std::cout << "3. Add At Position\n";
+    std::cout << "4. Remove First\n";
+    std::cout << "5. Remove Last\n";
+    std::cout << "6. Remove At Position\n";
+    std::cout << "7. Get At Position\n";
+    std::cout << "8. Get Size\n";
+    std::cout << "9. Is Empty\n";
+    std::cout << "10. Print List\n";
+    std::cout << "0. Exit\n";
+    std::cout << "Select option: ";
+}
+
+
 int main()
 {
-    std::cout << "Hello Word" << std::endl;
+    LinkedList<int> list;
+    int option = -1;
+
+    while (option != 0)
+    {
+        showMenu();
+        std::cin >> option;
+
+        try
+        {
+            switch (option)
+            {
+            case 1:
+                {
+                    int value;
+                    std::cout << "Value: ";
+                    std::cin >> value;
+                    list.addFirst(value);
+                    break;
+                }
+
+            case 2:
+                {
+                    int value;
+                    std::cout << "Value: ";
+                    std::cin >> value;
+                    list.addLast(value);
+                    break;
+                }
+
+            case 3:
+                {
+                    int value, pos;
+                    std::cout << "Value: ";
+                    std::cin >> value;
+                    std::cout << "Position: ";
+                    std::cin >> pos;
+                    list.addAt(value, pos);
+                    break;
+                }
+
+            case 4:
+                list.removeFirst();
+                break;
+
+            case 5:
+                list.removeLast();
+                break;
+
+            case 6:
+                {
+                    int pos;
+                    std::cout << "Position: ";
+                    std::cin >> pos;
+                    list.removeAt(pos);
+                    break;
+                }
+
+            case 7:
+                {
+                    int pos;
+                    std::cout << "Position: ";
+                    std::cin >> pos;
+                    auto node = list.getAt(pos);
+                    std::cout << "Value: " << list.getContent(node) <<
+
+                        std::endl;
+                    break;
+                }
+
+            case 8:
+                std::cout << "Size: " << list.getSize() << std::endl;
+                break;
+
+            case 9:
+                std::cout << (list.isEmpty()
+                                  ? "List is empty\n"
+                                  : "List is NOT empty\n");
+                break;
+
+            case 10:
+                {
+                    SimpleNode<int>* current = list.getHead();
+                    while (current != nullptr)
+                    {
+                        std::cout << current->value() << " -> ";
+                        current = current->next();
+                    }
+                    std::cout << "nullptr\n";
+                    break;
+                }
+
+            case 0:
+                std::cout << "Exiting...\n";
+                break;
+
+            default:
+                std::cout << "Invalid option\n";
+            }
+        }
+        catch (const std::exception& e)
+        {
+            std::cout << "Error: " << e.what() << std::endl;
+        }
+    }
+
     return 0;
 }
