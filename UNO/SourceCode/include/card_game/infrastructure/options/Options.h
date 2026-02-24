1bbf2214ca4e6a85a038c0ca4d05ddd2ae1c32b9
YmCris <ymcrisystems@gmail.com>
2026-02-23 21:56:18 -0600

    Adding main frontend

---
+++ UNO/SourceCode/include/card_game/infrastructure/options/Options.h
@@ -0,0 +1,23 @@
+//
+// Created by cristian on 2/23/26.
+//
+
+#ifndef SOURCECODE_OPTIONS_H
+#define SOURCECODE_OPTIONS_H
+
+
+/**
+* The Options class header is the class responsible for 
+* 
+* @author YmCris
+* @since 2/23/26
+*/
+class Options
+{
+public:
+    // PUBLIC API
+    void showOptions();
+};
+
+
+#endif //SOURCECODE_OPTIONS_H
